223a4695239e07f0962d657d4c39520828da552b
smabotics <smarobotics@bkeastmond.com>
2015-03-12 11:21:10 -0600

    The practice robot is Robot2.java

--- 2015Robot/2015Robot/src/org/usfirst/frc/team5493/robot/Robot.java
+++ 2015Robot/2015Robot/src/org/usfirst/frc/team5493/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -36,91 +37,47 @@ public class Robot extends SampleRobot {
 	RobotDrive myRobot; // class that handles basic drive operations
 	Joystick leftStick; // set to ID 1 in DriverStation
 	Joystick rightStick;
-	Trigger liftup;
-	Button liftdown;
 	Relay relay = new Relay(3);
 	private static final int DRIVERIGHT = 5;
 	private static final int DRIVELEFT = 1;
-	private static final int LIFTUP = 3;
-	// private static final int LIFTDOWN = 2;
-	CANTalon talon = new CANTalon(2);
+	CANTalon leftFront = new CANTalon(1);
+	CANTalon leftRear = new CANTalon(2);
+	CANTalon rightFront = new CANTalon(3);
+	CANTalon rightRear = new CANTalon(4);
+	TalonSRX put = new TalonSRX(1);
 	final DriverStation ds = DriverStation.getInstance();
+	boolean engaged = false;
 
 	public Robot() {
-		myRobot = new RobotDrive(1, 2);
+		myRobot = new RobotDrive(leftFront, leftRear, rightFront, rightRear);
 		myRobot.setExpiration(0.1);
 		leftStick = new Joystick(1);
-		// liftup = new Trigger();
-		// liftdown = new JoystickButton(leftStick, LIFTDOWN);
+
 
 	}
 
 	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
+		myRobot.isSafetyEnabled();
 		while (true && isOperatorControl() && isEnabled()) {
 			myRobot.tankDrive(leftStick.getRawAxis(DRIVERIGHT),
 					leftStick.getRawAxis(DRIVELEFT));
 
 			Timer.delay(0.005); // wait for a motor update time
 
-			if (leftStick.getRawButton(3)) {
-				ds.reportError("Button Clicked", false);
-				talon.changeControlMode(ControlMode.PercentVbus);
-				talon.set(.1);
-				Timer.delay(3);
-				talon.set(0);
-				Timer.delay(1);
-				talon.set(-.1);
-				Timer.delay(3);
-				talon.set(0);
+			if (leftStick.getRawButton(5)) {
+				DriverStation.reportError("DOWN", false);
+				put.set(.5);
+			}else if(leftStick.getRawButton(6)) {
+				DriverStation.reportError("UP", false);
+				put.set(-.5);
+			}else if(leftStick.getRawButton(1)) {
+				DriverStation.reportError("ENGAGED", false);
+				put.set(-.2);
+			}else{
+				DriverStation.reportError("Neg Button not clicked", false);
+						put.set(0);
+					}
 			}
-			// liftup.whenPressed(new Command(){
-			// boolean isbusy = false;
-			// @Override
-			// protected void initialize() {
-			// // TODO Auto-generated method stub
-			// talon.changeControlMode(ControlMode.PercentVbus);
-			// talon.set(1);
-			// }
-			//
-			// @Override
-			// protected void execute() {
-			// // TODO Auto-generated method stub
-			// isbusy = true;
-			// double selectedsensorspeed = talon.getSpeed();
-			// System.out.println("in execute");
-			// ds.reportError("in execute", false);
-			// Timer.delay(0.5);
-			// isbusy = false;
-			// }
-			//
-			// @Override
-			// protected boolean isFinished() {
-			// // TODO Auto-generated method stub
-			// return isbusy;
-			// }
-			//
-			// @Override
-			// protected void end() {
-			// // TODO Auto-generated method stub
-			//
-			// }
-			//
-			// @Override
-			// protected void interrupted() {
-			// // TODO Auto-generated method stub
-			//
-			// }});
-
-		}
-
-		// }else if(leftStick.getRawButton(LIFTDOWN)){
-		// relay.set(Value.kOn);
-		// relay.setDirection(Direction.kReverse);
-		// Timer.delay(3);
-		// relay.set(Value.kOff);
-		// }
-		//
-		// }
+	}	
 	}
-}
+
